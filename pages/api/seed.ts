90b71497e63f31a521db87d15301c46653a611a1
lionelritchie29 <lionelrtchieee@gmail.com>
2022-04-01 09:09:57 +0700

    feat: add seed api

---
+++ pages/api/seed.ts
@@ -0,0 +1,48 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (req.method === 'POST') {
+    const { key } = req.query;
+
+    if (key === process.env.NEXT_PUBLIC_SEED_KEY) {
+      await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/semesters`, {
+        startYear: 2021,
+        endYear: 2022,
+        type: 'Odd',
+        isActive: false,
+      });
+
+      await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/semesters`, {
+        startYear: 2021,
+        endYear: 2022,
+        type: 'Even',
+        isActive: true,
+      });
+
+      await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/roles`, {
+        roleName: 'Super Admin',
+      });
+
+      await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/roles`, {
+        roleName: 'Admin',
+      });
+
+      await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/roles`, {
+        roleName: 'User',
+      });
+
+      return res.json('Seed Success');
+    } else {
+      return res.json('Unauthorized');
+    }
+  }
+}
