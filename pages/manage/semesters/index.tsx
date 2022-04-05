35ca902727ea299f4700dfcb8f5d6c2a43ee0e55
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-04-05 14:31:19 +0700

    feat: manage semesters (view)

--- pages/manage/semesters/index.tsx
+++ pages/manage/semesters/index.tsx
@@ -1,8 +1,31 @@
+import { useAtom } from 'jotai';
 import { NextPage } from 'next';
+import { semestersAtom } from '../../../atom';
+import ManageSemestersTable from '../../../components/semesters/ManageSemestersTable';
 import Layout from '../../../components/shared/_Layout';
+import { SemestersService } from '../../../services/SemestersService';
 
 const ManageSemestersPage: NextPage = () => {
-  return <Layout>Hehe</Layout>;
+  const [semesters] = useAtom(semestersAtom);
+
+  return (
+    <Layout>
+      <div className='font-bold text-2xl mb-6'>
+        <h1>Manage Semesters</h1>
+      </div>
+      <ManageSemestersTable semesters={semesters} />
+    </Layout>
+  );
 };
+
+export async function getServerSideProps() {
+  const semesters = await SemestersService.getSemesters();
+
+  return {
+    props: {
+      semesters,
+    },
+  };
+}
 
 export default ManageSemestersPage;
