a221633c0d62867cac20d939b0329f88cf84499d
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-04-19 13:39:07 +0700

    feat: add auth helper to check user role

---
+++ lib/auth-helper.ts
@@ -0,0 +1,12 @@
+import { Session } from 'next-auth';
+import { SessionUser } from '../models/SessionUser';
+
+export const AuthHelper = {
+  isLoggedInAndHasRole(session: Session, allowedRoles: string[]) {
+    if (!session) return false;
+    const user = session.user as SessionUser;
+    if (!user) return false;
+    if (!allowedRoles.includes(user.roleName)) return false;
+    return true;
+  },
+};
