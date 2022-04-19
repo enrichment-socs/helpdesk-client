21a13f774a5a588c60c9d84c4371da6bb5c3956c
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-04-19 13:40:25 +0700

    refactor: remove secret in next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -63,5 +63,4 @@ export default NextAuth({
       },
     }),
   ],
-  secret: 'hesoyam',
 });
--- pages/manage/_middleware.ts
+++ pages/manage/_middleware.ts
@@ -1,10 +1,11 @@
 import { getToken } from 'next-auth/jwt';
+import { getSession } from 'next-auth/react';
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const url = req.nextUrl.clone();
 
-  const session = await getToken({ req: req as any, secret: 'hesoyam' });
+  const session = await getToken({ req: req as any });
   if (!session) {
     url.pathname = '/auth/login';
     return NextResponse.rewrite(url);
