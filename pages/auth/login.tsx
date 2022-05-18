29119937c1c5456f70afb5dc7d5cb8439b4f6157
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-05-18 11:44:07 +0700

    feat: redesign login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -4,8 +4,7 @@ import Layout from '../../widgets/_Layout';
 import ribbon from '../../public/assets/ribbon.png';
 import binus from '../../public/assets/binus.png';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { UserIcon, LockClosedIcon } from '@heroicons/react/solid';
 import {
@@ -18,7 +17,6 @@ import {
 import { signIn, getSession } from 'next-auth/react';
 import toast from 'react-hot-toast';
 import { SemestersService } from '../../services/SemestersService';
-import { withIronSessionSsr } from 'iron-session/next';
 import { withSessionSsr } from '../../shared/libs/session';
 import { getInitialServerProps } from '../../shared/libs/initialize-server-props';
 
@@ -72,98 +70,118 @@ const LoginPage: NextPage = () => {
     <Layout
       controlWidth={false}
       withNavbar={false}
-      className="flex justify-center items-center"
-      style={{ backgroundColor: '#259BE5' }}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="min-w-[21rem] rounded-lg border bg-white border-gray-200 overflow-hidden shadow-md">
-          <div className="flex items-center">
-            <div className="ml-3">
-              <Image src={ribbon} width={35} height={120} alt="" />
-            </div>
-            <div className="ml-3">
-              <Image src={binus} width={120} height={70} alt="" />
-            </div>
-          </div>
-
-          <div className="p-8">
-            <div className="mt-1 relative rounded-md shadow-sm">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <UserIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </div>
-              <input
-                type="text"
-                {...register('username', {
-                  required: true,
-                  validate: (value) =>
-                    STUDENT_EMAIL_REGEX.test(value) ||
-                    EMPLOYEE_EMAIL_REGEX.test(value) ||
-                    STUDENT_NUMBER_REGEX.test(value) ||
-                    AST_INITIAL_REGEX.test(value) ||
-                    value === ADMIN_USERNAME,
-                })}
-                className={`outline-none block w-full pl-10 sm:text-sm p-2 border rounded-md ${
-                  errors.username ? 'border-red-300' : 'border-gray-300'
-                }`}
-                placeholder="Username "
-              />
-            </div>
-            {errors.username?.type === 'required' && (
-              <small className="text-red-500">Username must be filled</small>
-            )}
-            {errors.username?.type === 'validate' && (
-              <small className="text-red-500">
-                Username must be a valid NIM or Binus Email
-              </small>
-            )}
-
-            <div className="mt-2 relative rounded-md shadow-sm">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <LockClosedIcon
-                  className="h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </div>
-              <input
-                type="password"
-                {...register('password', { required: true })}
-                className={`outline-none block w-full pl-10 sm:text-sm p-2 border rounded-md ${
-                  errors.password ? 'border-red-300' : 'border-gray-300'
-                }`}
-                placeholder="Password"
-              />
-            </div>
-            {errors.password && (
-              <small className="text-red-500">Password must be filled</small>
-            )}
-
-            <button
-              type="submit"
-              disabled={isLoading}
-              className={`mt-4 inline-flex w-full justify-center items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white ${
-                isLoading ? 'bg-gray-400' : 'bg-primary hover:bg-primary-dark'
-              }`}>
-              Login
-            </button>
-          </div>
-        </div>
-
-        <div className="space-y-2 bg-white rounded-lg p-4 h-full max-w-[21rem] mt-4 text-sm shadow-md">
-          <h1>Note</h1>
+      className="flex flex-col-reverse md:flex-row justify-center items-center md:space-x-8"
+      style={{
+        backgroundImage: `url('/assets/login_backdrop.jpg')`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}>
+      <div className="w-full md:w-1/2 flex justify-center md:justify-end items-center">
+        <div className="max-w-[21rem] text-gray-300 text-sm md:text-base mt-4 md:mt-0">
+          <h1 className="font-bold">Note</h1>
           <ul className="space-y-2">
             <li>
-              - <b>Employee</b>: Use your <b>binus.edu</b> email for username
-              and the password is the same as your email password
+              - <b>For Employee</b>: Use your <b>binus.edu</b> email for
+              username and the password is the same as your email password
             </li>
             <li>
-              - <b>Student</b>: Use your <b>NIM</b> as username and the password
-              is the same as your Binusmaya password
+              - <b>For Student</b>: Use your <b>NIM</b> as username and the
+              password is the same as your Binusmaya password
             </li>
           </ul>
+
+          <small className="block mt-4">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+              href="https://www.freepik.com/vectors/memphis-shapes">
+              Background Attribution
+            </a>
+          </small>
         </div>
-      </form>
+      </div>
+      <div className="w-full md:w-1/2">
+        <form
+          className="flex justify-center md:justify-start items-center px-4"
+          onSubmit={handleSubmit(onSubmit)}>
+          <div className="min-w-[21rem] rounded border bg-white border-gray-200 overflow-hidden shadow-lg">
+            <div className="flex items-center">
+              <div className="ml-3">
+                <Image src={ribbon} width={35} height={120} alt="" />
+              </div>
+              <div className="ml-3">
+                <Image src={binus} width={120} height={70} alt="" />
+              </div>
+            </div>
+
+            <div className="p-8">
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <UserIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+                <input
+                  type="text"
+                  {...register('username', {
+                    required: true,
+                    validate: (value) =>
+                      STUDENT_EMAIL_REGEX.test(value) ||
+                      EMPLOYEE_EMAIL_REGEX.test(value) ||
+                      STUDENT_NUMBER_REGEX.test(value) ||
+                      AST_INITIAL_REGEX.test(value) ||
+                      value === ADMIN_USERNAME,
+                  })}
+                  className={`outline-none block w-full pl-10 sm:text-sm p-2 border rounded-md ${
+                    errors.username ? 'border-red-300' : 'border-gray-300'
+                  }`}
+                  placeholder="Username "
+                />
+              </div>
+              {errors.username?.type === 'required' && (
+                <small className="text-red-500">Username must be filled</small>
+              )}
+              {errors.username?.type === 'validate' && (
+                <small className="text-red-500">
+                  Username must be a valid NIM or Binus Email
+                </small>
+              )}
+
+              <div className="mt-2 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+                <input
+                  type="password"
+                  {...register('password', { required: true })}
+                  className={`outline-none block w-full pl-10 sm:text-sm p-2 border rounded-md ${
+                    errors.password ? 'border-red-300' : 'border-gray-300'
+                  }`}
+                  placeholder="Password"
+                />
+              </div>
+              {errors.password && (
+                <small className="text-red-500">Password must be filled</small>
+              )}
+
+              <button
+                type="submit"
+                disabled={isLoading}
+                className={`mt-4 inline-flex w-full justify-center items-center px-4 py-3 border border-transparent text-sm leading-4 font-medium rounded shadow-sm text-white ${
+                  isLoading ? 'bg-gray-400' : 'bg-primary hover:bg-primary-dark'
+                }`}>
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 };
