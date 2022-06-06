e28f5d728e45a8699297d0184a09698db0584b75
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-06-06 14:02:33 +0700

    chore: remove unused code (bimay service)

--- services/BaseService.ts
+++ services/BaseService.ts
@@ -5,11 +5,6 @@ import { PromiseWrapper } from '../shared/libs/promise-wrapper';
 
 export class BaseService {
   protected BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
-  protected BINUS_BASE_URL: string =
-    process.env.NEXT_PUBLIC_BINUS_BASE_API_URL!;
-  protected DIVISON_ID: string = process.env.NEXT_PUBLIC_DIVISON_ID!;
-  protected BINUSMAYA_LOGIN_API_URL =
-    process.env.NEXT_PUBLIC_BINUSMAYA_LOGIN_API!;
 
   protected accessToken: string = '';
   protected wrapper: PromiseWrapper;
