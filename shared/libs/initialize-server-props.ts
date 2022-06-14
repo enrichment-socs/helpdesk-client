5538e4dda407656e30c9834daa1535dd039798ee
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-06-14 10:25:44 +0700

    feat: fix unauthorized error on server side

--- shared/libs/initialize-server-props.ts
+++ shared/libs/initialize-server-props.ts
@@ -3,6 +3,8 @@ import { Session } from 'next-auth';
 import { GetSessionParams } from 'next-auth/react';
 import { Semester } from '../../models/Semester';
 import { SemesterService } from '../../services/SemesterService';
+import jwt_decode from 'jwt-decode';
+import { SessionUser } from '../../models/SessionUser';
 
 type Props = {
   session: Session;
@@ -17,8 +19,8 @@ export const getInitialServerProps = async (
   semesterService: SemesterService
 ): Promise<Props> => {
   const semesters = await semesterService.getSemesters();
-  const session = await getSession({ req });
   const dbActiveSemester = semesters.find((s) => s.isActive);
+  let session = await getSession({ req });
   let activeSemester = req.session.activeSemester ?? dbActiveSemester;
 
   if (!req.session.activeSemester) {
@@ -30,6 +32,14 @@ export const getInitialServerProps = async (
       req.session.activeSemester = dbActiveSemester;
       await req.session.save();
     }
+  }
+
+  if (session) {
+    const user = session?.user as SessionUser;
+    const payload: any = jwt_decode(user?.accessToken);
+    const expDate = new Date(payload.exp * 1000);
+    const now = new Date();
+    if (now >= expDate) session = null;
   }
 
   return {
