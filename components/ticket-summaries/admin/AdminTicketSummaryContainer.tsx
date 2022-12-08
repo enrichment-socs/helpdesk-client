43afa004dedf8a76e54f9506f51db323fb19ceb5
wmartin16 <65394426+wmartin16@users.noreply.github.com>
2022-12-08 16:36:16 +0900

    update: add colors in ticket summary

--- components/ticket-summaries/admin/AdminTicketSummaryContainer.tsx
+++ components/ticket-summaries/admin/AdminTicketSummaryContainer.tsx
@@ -1,5 +1,6 @@
 import { ChartBarIcon } from '@heroicons/react/solid';
 import { TicketSummary } from '../../../models/TicketSummary';
+import { COLORS } from '../../../shared/constants/color';
 
 type Props = {
   ticketSummary: TicketSummary;
@@ -10,22 +11,27 @@ const AdminTicketSummaryContainer = ({ ticketSummary }: Props) => {
     {
       name: 'Assigned',
       count: ticketSummary.assignedCount,
+      color: COLORS.ASSIGNED_TEXT_COLOR,
     },
     {
       name: 'Pending',
       count: ticketSummary.pendingCount,
+      color: COLORS.PENDING_TEXT_COLOR,
     },
     {
       name: 'In Progress',
       count: ticketSummary.inProgressCount,
+      color: COLORS.INPROGRESS_TEXT_COLOR,
     },
     {
       name: 'Resolved',
       count: ticketSummary.resolvedCount,
+      color: COLORS.RESOLVED_TEXT_COLOR,
     },
     {
       name: 'Closed',
       count: ticketSummary.closedCount,
+      color: COLORS.CLOSED_TEXT_COLOR,
     },
   ];
 
@@ -40,7 +46,7 @@ const AdminTicketSummaryContainer = ({ ticketSummary }: Props) => {
         return (
           <div key={idx} className="p-3">
             <div className="font-semibold">{category.name}</div>
-            <div className="text-slate-600 text-4xl">{category.count}</div>
+            <div className="text-slate-600 text-4xl" style={{color: category.color}}>{category.count}</div>
           </div>
         );
       })}
