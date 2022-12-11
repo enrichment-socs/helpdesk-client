ea77bfdc48fbd87f1dfbb4bb1eae05356ba93dde
lionelritchie29 <lionelrtchieee@gmail.com>
2022-12-11 15:43:12 +0700

    refactor: start use ReactTable for manage announcement page

--- components/announcements/ManageAnnouncementsTable.tsx
+++ components/announcements/ManageAnnouncementsTable.tsx
@@ -7,15 +7,74 @@ import { format } from 'date-fns';
 import { useSession } from 'next-auth/react';
 import { SessionUser } from '../../models/SessionUser';
 import ManageAnnouncementStore from '../../stores/manage/announcements';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 
 type Props = {
   openModal: (announcement: Announcement | null) => void;
 };
 
-export default function ManageAnnouncementsTable({
-  openModal,
-}: Props) {
-  const [announcements, setAnnouncements] = useAtom(ManageAnnouncementStore.announcements);
+export default function ManageAnnouncementsTable({ openModal }: Props) {
+  const [announcements, setAnnouncements] = useAtom(
+    ManageAnnouncementStore.announcements
+  );
+
+  const columnHelper = createColumnHelper<Announcement>();
+  const columns = [
+    columnHelper.accessor('title', {
+      cell: (info) => info.getValue(),
+      header: 'Title',
+    }),
+    columnHelper.accessor('role', {
+      cell: (info) => {
+        const role = info.row.original.role;
+        return role ? role.roleName : 'All';
+      },
+      header: 'Target Role',
+    }),
+    columnHelper.accessor('startDate', {
+      cell: (info) => (
+        <>{format(new Date(info.getValue()), 'yyyy-MM-dd HH:mm')}</>
+      ),
+      header: 'Start Date',
+    }),
+    columnHelper.accessor('endDate', {
+      cell: (info) => (
+        <>{format(new Date(info.getValue()), 'yyyy-MM-dd HH:mm')}</>
+      ),
+      header: 'End Date',
+    }),
+    columnHelper.display({
+      id: 'action',
+      cell: (info) => (
+        <div className="flex space-x-2">
+          <button
+            type="button"
+            onClick={() => openModal(info.row.original)}
+            className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Update
+          </button>
+          <button
+            type="button"
+            onClick={() => onDelete(info.row.original)}
+            className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+            Delete
+          </button>
+        </div>
+      ),
+      header: '',
+    }),
+  ];
+
+  const table = useReactTable({
+    data: announcements,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   const session = useSession();
   const user = session?.data?.user as SessionUser;
@@ -44,34 +103,21 @@ export default function ManageAnnouncementsTable({
     <div className="flex flex-col">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <div className="shadow overflow-hidden border-b border-gray-200 rounded">
             <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-primary">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                    Announcement Title
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                    Target Role
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                    Start Date
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                    End Date
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
-                    <span className="sr-only">Edit</span>
-                  </th>
-                </tr>
+              <thead className="bg-gray-500">
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <tr key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <th
+                        key={header.id}
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                        {header.column.columnDef.header}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
               </thead>
               <tbody>
                 {announcements.length == 0 && (
@@ -81,45 +127,22 @@ export default function ManageAnnouncementsTable({
                     </td>
                   </tr>
                 )}
-                {announcements &&
-                  announcements.map((announcement, idx) => (
-                    <tr
-                      key={announcement.id}
-                      className={idx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {announcement.title}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {announcement.role ? announcement.role.roleName : 'All'}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {format(
-                          new Date(announcement.startDate),
-                          'yyyy-MM-dd HH:mm'
+                {table.getRowModel().rows.map((row, idx) => (
+                  <tr
+                    className={idx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
+                    key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <td
+                        key={cell.id}
+                        className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
                         )}
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {format(
-                          new Date(announcement.endDate),
-                          'yyyy-MM-dd HH:mm'
-                        )}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-1">
-                        <button
-                          type="button"
-                          onClick={() => openModal(announcement)}
-                          className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                          Update
-                        </button>
-                        <button
-                          type="button"
-                          onClick={() => onDelete(announcement)}
-                          className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
+                    ))}
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
