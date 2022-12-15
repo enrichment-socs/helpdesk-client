4c23b8382b9f72126649fd455582de7fa35ad2df
lionelritchie29 <lionelrtchieee@gmail.com>
2022-12-15 14:21:20 +0700

    refactor: use react table for ticket table

--- components/tickets/TicketTable.tsx
+++ components/tickets/TicketTable.tsx
@@ -3,6 +3,13 @@ import { differenceInHours, format } from 'date-fns';
 import { Ticket } from '../../models/Ticket';
 import { STATUS } from '../../shared/constants/status';
 import { PRIORITY } from '../../shared/constants/priority';
+import clsx from 'clsx';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 
 type Props = {
   tickets: Ticket[];
@@ -15,6 +22,52 @@ const TicketTable: React.FC<Props> = ({
   showLegends = true,
   forPending = false,
 }) => {
+  const columnHelper = createColumnHelper<Ticket>();
+  const columns = [
+    // columnHelper.display({
+    //   cell: (info) => `${startNumber + info.row.index}`,
+    //   header: 'No',
+    // }),
+    columnHelper.accessor('subject', {
+      cell: (info) => info.getValue() || 'No Subject',
+      id: 'subject',
+      header: 'Subject',
+    }),
+    columnHelper.accessor('senderName', {
+      cell: (info) => info.getValue(),
+      id: 'senderName',
+      header: 'Requester Name',
+    }),
+    columnHelper.accessor('assignedTo.name', {
+      cell: (info) => info.getValue(),
+      header: 'Assigned To',
+    }),
+    columnHelper.accessor('dueBy', {
+      cell: (info) =>
+        `${format(new Date(info.getValue()), 'dd MMM yyyy, kk:mm')}`,
+      header: `Due By ${forPending ? '(Freezed)' : ''}`,
+    }),
+    columnHelper.accessor('status.statusName', {
+      cell: (info) => info.getValue(),
+      header: 'Status',
+    }),
+    columnHelper.accessor('priority.priorityName', {
+      cell: (info) => info.getValue(),
+      header: 'Priority',
+    }),
+    columnHelper.accessor('created_at', {
+      cell: (info) =>
+        `${format(new Date(info.getValue()), 'dd MMM yyyy, kk:mm')}`,
+      header: 'Created At',
+    }),
+  ];
+
+  const table = useReactTable({
+    data: tickets,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   const router = useRouter();
   const legends = [
     {
@@ -84,89 +137,59 @@ const TicketTable: React.FC<Props> = ({
             <div className="shadow overflow-auto border-b border-gray-200">
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-500">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                      Subject
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                      Requester Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                      Assigned To
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                      Due By {forPending ? '(Freezed)' : ''}
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                      Status
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                      Priority
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-                      Created At
-                    </th>
-                  </tr>
+                  {table.getHeaderGroups().map((headerGroup) => (
+                    <tr key={headerGroup.id}>
+                      {headerGroup.headers.map((header) => (
+                        <th
+                          key={header.id}
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                          {header.column.columnDef.header}
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
                 </thead>
                 <tbody>
-                  {tickets.length == 0 && (
-                    <tr className="text-center">
-                      <td colSpan={7} className="p-4 text-center">
-                        There are currently no tickets
+                  {table.getRowModel().rows.length == 0 && (
+                    <tr>
+                      <td
+                        colSpan={table.getHeaderGroups()[0].headers.length}
+                        className="text-center p-4">
+                        There are currently no ticket
                       </td>
                     </tr>
                   )}
 
-                  {tickets &&
-                    tickets.map((data, index) => (
+                  {table.getRowModel().rows.map((row, idx) => {
+                    return (
                       <tr
-                        key={data.id}
-                        className={`cursor-pointer ${
-                          index % 2 === 0 ? 'bg-white' : 'bg-gray-50 '
-                        } transition duration-300 hover:bg-sky-100 ease-in-out ${getRowBgColor(
-                          data
-                        )}`}
-                        onClick={rowClickHandler.bind(this, data.id)}>
-                        <td className="max-w-[20rem] truncate px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {data.subject || 'No Subject'}
-                        </td>
-                        <td className="max-w-[16rem] truncate px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {data.senderName}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {data.assignedTo.name}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {format(new Date(data.dueBy), 'dd MMM yyyy, kk:mm')}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {data.status.statusName}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {data.priority.priorityName}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {format(
-                            new Date(data.created_at),
-                            'dd MMM yyyy, kk:mm'
-                          )}
-                        </td>
+                        className={clsx(
+                          'transition duration-300 ease-in-out hover:bg-sky-100 cursor-pointer transition duration-300 hover:bg-sky-100 ease-in-out',
+                          getRowBgColor(row.original)
+                        )}
+                        onClick={rowClickHandler.bind(this, row.original)}
+                        key={row.id}>
+                        {row.getVisibleCells().map((cell) => (
+                          <td
+                            key={cell.id}
+                            className={clsx(
+                              'px-6 py-4 whitespace-nowrap text-sm font-medium truncate',
+                              {
+                                'max-w-[16rem]':
+                                  cell.column.id === 'senderName',
+                              },
+                              { 'max-w-[20rem]': cell.column.id === 'subject' }
+                            )}>
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </td>
+                        ))}
                       </tr>
-                    ))}
+                    );
+                  })}
                 </tbody>
               </table>
             </div>
