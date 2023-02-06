19808a164e255f599ec0d9a6c509adec86c4215e
lionelritchie29 <lionelrtchieee@gmail.com>
2023-02-06 11:09:54 +0700

    feat: update login background url

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -72,7 +72,7 @@ const LoginPage: NextPage = () => {
       withNavbar={false}
       className="flex flex-col-reverse md:flex-row justify-center items-center md:space-x-8"
       style={{
-        backgroundImage: `url('/helpdesk/assets/login_backdrop.jpg')`,
+        backgroundImage: `url('${process.env.NEXT_PUBLIC_BASE_PATH}/assets/login_backdrop.jpg')`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
